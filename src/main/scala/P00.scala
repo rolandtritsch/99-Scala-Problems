c0daad26886a8e715734c156980d972399d5f550
Roland Tritsch <roland.tritsch@gonitro.com>
2017-03-28 09:07:54 +0100

    P01-P03: merge

--- src/main/scala/P00.scala
+++ src/main/scala/P00.scala
@@ -2,6 +2,8 @@ package org.p99.scala
 
 object P00 {
   def main(args: Array[String]): Unit = {
-    P01.run()
+    assert(P01.last(List(1, 1, 2, 3, 5, 8)) == 8)
+    assert(P02.penultimate(List(1, 1, 2, 3, 5, 8)) == 5)
+    assert(P03.nth(2, List(1, 1, 2, 3, 5, 8)) == 2)
   }
 }
--- src/test/scala/P01Spec.scala
+++ src/test/scala/P01Spec.scala
@@ -5,22 +5,9 @@ import org.scalatest._
 class P01Spec extends FlatSpec {
   import P01._
 
-<<<<<<< HEAD
   "last(List(...)" should "return the last element of the list" in {
     assert(last(List(1, 1, 2, 3, 5, 8)) === 8)
   }
-
-  it should "throw an exception when invoked with an empty list" in {
-    assertThrows[java.lang.IllegalArgumentException] {
-      last(List())
-    }
-  }
-}
-=======
-  "last(List(...))" should "return the last element of a list" in {
-    assert(last(List(1, 1, 2, 3, 5, 8)) === 8)
-  }
->>>>>>> 8b60620... P01-P03: Initial commit
 
   it should "throw an exception when invoked with an empty list" in {
     assertThrows[java.lang.IllegalArgumentException] {
