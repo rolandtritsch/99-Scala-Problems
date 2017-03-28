54b0cff7438f845af733b1aba0aceb56b8854303
Roland Tritsch <roland.tritsch@gonitro.com>
2017-03-28 13:49:14 +0100

    P05-P07: Initial commit

--- src/main/scala/P06.scala
+++ src/main/scala/P06.scala
@@ -1,7 +1,12 @@
 package org.p99.scala
 
 object P06 {
-
-  // TODO
-
+  /** Find out whether a list is a palindrome.
+    *
+    * @param l - the list
+    * @return True, if the list is a palindroms
+    */
+  def isPalindrome(l: List[Char]): Boolean = {
+    l == l.reverse
+  } ensuring(_ || l != l.reverse)
 }
--- src/test/scala/P06Spec.scala
+++ src/test/scala/P06Spec.scala
@@ -2,9 +2,23 @@ package org.p99.scala
 
 import org.scalatest._
 
-class P06Spec extends UnitSpec {
+class P06Spec extends FlatSpec {
+  import P06._
 
-  // TODO
+  "palindrome(List(...)" should "return true for a palindrome" in {
+    assert(isPalindrome(List(1, 2, 3, 2, 1)))
+  }
 
+  it should "return true for a palindrome (string)" in {
+    assert(isPalindrome("racecar".toList))
+  }
+
+  it should "return false if the list is not a palindrome" in {
+    assert(!isPalindrome(List(9999, 1, 2, 3, 2, 1)))
+  }
+
+  it should "return true for an empty list" in {
+    assert(isPalindrome(List()))
+  }
 }
 
