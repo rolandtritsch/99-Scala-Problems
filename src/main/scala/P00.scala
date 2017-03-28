346a8f92eb0ddada379e6aed66e9a4aeb4fe856c
Roland Tritsch <roland.tritsch@gonitro.com>
2017-03-28 09:13:23 +0100

    Beautify P00

--- src/main/scala/P00.scala
+++ src/main/scala/P00.scala
@@ -2,8 +2,9 @@ package org.p99.scala
 
 object P00 {
   def main(args: Array[String]): Unit = {
-    assert(P01.last(List(1, 1, 2, 3, 5, 8)) == 8)
-    assert(P02.penultimate(List(1, 1, 2, 3, 5, 8)) == 5)
-    assert(P03.nth(2, List(1, 1, 2, 3, 5, 8)) == 2)
+    print("P01"); assert(P01.last(List(1, 1, 2, 3, 5, 8)) == 8)
+    print("/P02"); assert(P02.penultimate(List(1, 1, 2, 3, 5, 8)) == 5)
+    print("/P03"); assert(P03.nth(2, List(1, 1, 2, 3, 5, 8)) == 2)
+    println()
   }
 }
