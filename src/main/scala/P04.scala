0d038b0ed4e62c5405fb0652e280dc394a9a43c2
Roland Tritsch <roland.tritsch@gonitro.com>
2017-03-28 09:20:15 +0100

    P04: Initial commit

--- src/main/scala/P04.scala
+++ src/main/scala/P04.scala
@@ -1,7 +1,12 @@
 package org.p99.scala
 
 object P04 {
-
-  // TODO
-
+  /** Find the number of elements of a list.
+    *
+    * @param l the list
+    * @return the number of elements in the list
+    */
+  def length(l: List[_]): Int = {
+    l.size
+  } ensuring(_ == l.size)
 }
--- src/test/scala/P04Spec.scala
+++ src/test/scala/P04Spec.scala
@@ -2,9 +2,14 @@ package org.p99.scala
 
 import org.scalatest._
 
-class P04Spec extends UnitSpec {
+class P04Spec extends FlatSpec {
+  import P04._
 
-  // TODO
+  "length(List(...)" should "return the size of the list" in {
+    assert(length(List(1, 1, 2, 3, 5, 8)) === 6)
+  }
 
+  it should "return 0 for empty Lists" in {
+    assert(length(List()) === 0)
+  }
 }
-
