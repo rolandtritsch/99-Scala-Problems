aac16802cdcf1db5ad4677ddd8a2dd46ad35d087
Roland Tritsch <roland@gonito.com>
2017-08-03 09:09:32 +0100

    P29-P30: Initial commit

--- src/main/scala/P30.scala
+++ src/main/scala/P30.scala
@@ -2,6 +2,15 @@ package org.p99.scala
 
 object P30 {
 
-  // TODO
-
+  /** Again, we suppose that a list contains elements that are lists themselves. But
+    * this time the objective is to sort the elements according to their length
+    * frequency; i.e. in the default, sorting is done ascendingly, lists with rare
+    * lengths are placed first, others with a more frequent length come later.
+    *
+    * @param l The list to sort
+    * @return The sorted list
+    */
+  def lsortFreq(l: List[List[_]]): List[List[_]] = {
+    l.groupBy(_.size).toList.sortBy(_._2.size).flatMap(_._2)
+  }
 }
--- src/test/scala/P30Spec.scala
+++ src/test/scala/P30Spec.scala
@@ -2,9 +2,22 @@ package org.p99.scala
 
 import org.scalatest._
 
-class P30Spec extends UnitSpec {
+class P30Spec extends FlatSpec {
+  import P30._
 
-  // TODO
+  "lsortFreq(..)" should "return an empty list, when given an empty list" in {
+    assert(lsortFreq(List()) === List())
+  }
 
+  "lsortFreq(..)" should "return the list, when given a list of one list" in {
+    val l = List(List('a, 'b, 'c))
+    assert(lsortFreq(l) === l)
+  }
+
+  "lsortFreq(..)" should "return the sorted list, for a given list of lists" in {
+    val l = List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))
+    val result = List(List('i, 'j, 'k, 'l), List('o), List('a, 'b, 'c), List('f, 'g, 'h), List('d, 'e), List('d, 'e), List('m, 'n))
+    assert(lsortFreq(l) === result)
+  }
 }
 
