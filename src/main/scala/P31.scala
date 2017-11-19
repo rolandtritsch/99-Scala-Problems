0143ac11f56d0f30fb958f078915f79bcd3b9077
Roland Tritsch <roland@gonito.com>
2017-11-19 20:02:38 +0000

    P31: Inital commit

--- src/main/scala/P31.scala
+++ src/main/scala/P31.scala
@@ -2,6 +2,28 @@ package org.p99.scala
 
 object P31 {
 
-  // TODO
-
+  /** Determine whether a given integer number is prime.
+    *
+    * https://en.wikipedia.org/wiki/Primality_test
+    *
+    * @param p - The number to check
+    * @return If p is prime or not
+    */
+  def isPrime(p: Int): Boolean = {
+    require(p > 0, s"p is ${p}; needs to be > 0")
+    p match {
+      case p if(p <= 3) => true
+      case p if(p % 2 == 0) => false
+      case p if(p % 3 == 0) => false
+      case p => {
+        var i = 5
+        var done = false
+        while(p >= i * i && !done) {
+          if(p % i == 0 || p % (i + 2) == 0) done = true
+          else i = i + 6
+        }
+        if(done) false else true
+      }
+    }
+  }
 }
--- src/test/scala/P31Spec.scala
+++ src/test/scala/P31Spec.scala
@@ -2,9 +2,18 @@ package org.p99.scala
 
 import org.scalatest._
 
-class P31Spec extends UnitSpec {
+class P31Spec extends FlatSpec {
+  import P31._
 
-  // TODO
+  "isPrime(...)" should "throw an exception, if p is <= 0" in {
+    assertThrows[IllegalArgumentException] {
+      isPrime(0)
+    }
+  }
 
+  it should "return true, for all prime numbers" in {
+    val ps = List(1, 2, 3, 5, 7, 11, 13, 17, 19, 997)
+    assert(ps.forall(isPrime(_)))
+  }
 }
 
